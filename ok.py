758929c642ba380c014012dc602f4dc9a8b36da6
YassirMr <yassir.mr1@gmail.com>
2017-07-15 21:06:09 +0200

    A  newer version

--- ok.py
+++ ok.py
@@ -5,6 +5,6 @@ conn = sqlite3.connect('db.sqlite3')
 cursor = conn.cursor()
 
 x=35532-34163
-for i in range(46484-x,46484+1):
+for i in range(1,1369*2+1):
  cursor.execute('DELETE FROM heatmap_input WHERE id={}'.format(i))
 conn.commit()
